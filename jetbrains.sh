1bbe4a3662d323c299e44a0de9fe47e75e3450d8
mvargas <mvargas@technisys.com>
2019-08-18 21:51:32 -0400

    Intellij IDEA

--- jetbrains.sh
+++ jetbrains.sh
@@ -1,6 +1,13 @@
-# GoLand2019.1
-echo "Resetting Rubymine"
+# Intellij IDEA
+echo "Resetting Intellij IDEA"
+rm $HOME/Library/Preferences/jetbrains.idea.*.plist
+rm $HOME/Library/Preferences/IntelliJIdea2019.1/eval/*.key
+sed -i'' -e '/evlsprt/d' $HOME/Library/Preferences/IntelliJIdea2019.1/options/other.xml
+echo "Intellij IDEA done!"
+
+# GoLand
+echo "Resetting Golang"
 rm $HOME/Library/Preferences/jetbrains.goland.*.plist
 rm $HOME/Library/Preferences/GoLand2019.1/eval/*.key
 sed -i'' -e '/evlsprt/d' $HOME/Library/Preferences/GoLand2019.1/options/other.xml
-echo "Done!"
+echo "Golang done!"
